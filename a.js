88011a83dd6410b8f6633bd801087e8f795f9ebb
dangwalgourav200-cell <dangwalgourav200@gmail.com>
2026-03-16 01:10:34 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,11 @@
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+
+
+btn1.addEventListener('click', () => {
+    
+    window.location.href ="./dhan1.html"
+
+
+
+})
---
+++ x.js
@@ -0,0 +1,18 @@
+
+
+let btn = document.getElementById("button");
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+
+
+
+btn.addEventListener('click', () => {
+    
+    window.location.href ="a.html";
+})
+
+
+
+
+
+
